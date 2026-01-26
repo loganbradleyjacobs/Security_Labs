2679f52b3f91428cfa4b452355de929c2ec36952
lbj1533 <lxbxjx15@outlook.com>
2026-01-26 17:05:44 -0500

    attempting to add dunder methods to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,10 +22,32 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "sphinx_autodoc_typehints",
+    "sphinx.ext.githubpages",
 ]
 
+# Napoleon settings (for docstring parsing)
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = True
+napoleon_include_special_with_doc = True  # THIS IS CRITICAL - includes __contains__, __getitem__
+napoleon_use_ivar = True
+
+# Autodoc settings - these control what gets documented
+autodoc_default_options = {
+    'members': True,           # Include class members
+    'member-order': 'bysource', # Order as in source code
+    'special-members': '__init__, __contains__, __getitem__, __str__',
+    'undoc-members': True,     # Include members without docstrings
+    'exclude-members': '__weakref__',  # Exclude this internal member
+    'show-inheritance': True,  # Show class inheritance
+}
+autodoc_typehints = 'description'
+
 templates_path = ['_templates']
-exclude_patterns = []
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 
@@ -34,3 +56,13 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_show_sphinx = False
+html_show_copyright = True
+# If using Read the Docs theme
+html_theme_options = {
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
